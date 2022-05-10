569e9bffb16e2a602a007b6a4824e39c6dfe5d3f
jamin12 <rudals9901@naver.com>
2022-05-11 03:33:54 +0900

    DOM이용해 html객체 사용

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
-
+// https://www.samsungsds.com/kr/insights/TypeScript.html
 // const express = require('express');
 const app = express();
 
@@ -10,6 +10,7 @@ const home = require("./src/routes/home");
 app.set("views","./src/views");
 app.set("view engine","ejs");
 
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/",home); // use 미들 웨어를 등록해주는 메서드
 
 module.exports = app;
---
+++ app/src/public/ts/home/login.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const id = document.querySelector("#id");
+const pw = document.querySelector("#pw");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click",login);
+
+function login() {
+    const req = {
+        id : id.value,
+        pw : pw.value
+    };
+
+    console.log(req);
+}
