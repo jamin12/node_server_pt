a71b2c44f7e356a6e9c9f86d47887d8327eb1e44
jamin12 <rudals9901@naver.com>
2022-06-30 00:20:33 +0900

    Feat : add login request

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,22 @@
 "use strict";
 
-console.log("hello");
-console.log("bye")
+const id = document.querySelector("#id"),
+    pw = document.querySelector("#pw"),
+    loginbtn = document.querySelector("button");
+
+loginbtn.addEventListener("click",login);
+
+function login() {
+    const req = {
+        id : id.value,
+        pw : pw.value
+    };
+
+    fetch("/login",{
+        method : "POST",
+        headers : {
+            "Content-Type": "application/json"
+        },
+        body : JSON.stringify(req)
+    });
+}
